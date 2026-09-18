b7c78e2d7d83f6d18aaea4827e9b8f7cd3e5a7dc
Genilson dos Santos Filho <genilsondossantosfilho864@gmail.com>
2026-09-18 15:51:42 -0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,16 @@
+create table bolsaFamiliaPagamentos(
+MES_COMPETENCIA varchar(8),
+MES_REFERENCIA varchar(8),
+UF varchar(2),
+CODIGO_MUNICIPIO_SIAFI varchar(5),
+NOME_MUNICIPIO varchar(100),
+CPF_FAVORECIDO varchar(11),
+NIS_FAVORECIDO varchar(11),
+NOME_FAVORECIDO varchar(100),
+VALOR_PARCELA varchar(10)
+);
+
+
+
+
+																																																																		
